685c4623a2090fed281bba1c84afe3e71cb48a74
Dan Maas <dmaas@maasdigital.com>
2019-08-16 16:34:14 -0700

    remove divergence from master in loadCubeTextureAsync

--- build/loadCubeTextureAsync.js
+++ build/loadCubeTextureAsync.js
@@ -2,7 +2,6 @@ import CubeTexture from './CubeTexture';
 export default async function loadCubeTextureAsync(options) {
     const texture = new CubeTexture();
     await texture.loadAsync(options);
-    texture.image[0].isDataTexture = true; // DJM - necessary to upload the images via glTexture2D
     return texture;
 }
 //# sourceMappingURL=loadCubeTextureAsync.js.map
